90bd1d95df8ed03700ae9a0bfc553641692862d0
Tin Tadić <tin.tadic46@gmail.com>
2023-08-18 22:14:27 +0200

    fix migration data types

--- src/main/resources/db/migration/V1__create_processors_schema.sql
+++ src/main/resources/db/migration/V1__create_processors_schema.sql
@@ -3,11 +3,11 @@ CREATE TABLE IF NOT EXISTS `processors` (
     `name` varchar(100) NOT NULL UNIQUE,
     `manufacturer_name` varchar(100) DEFAULT NULL,
     `socket` varchar(100) DEFAULT NULL,
-    `release_date` TIMESTAMP DEFAULT NULL,
+    `release_date` TIMESTAMP,
     `number_of_cores` int(11) DEFAULT NULL,
     `number_of_threads` int(11) DEFAULT NULL,
-    `base_clock_speed` int(11) DEFAULT NULL,
-    `boost_clock_speed` int(11) DEFAULT NULL,
-    `retail_price` int(11) DEFAULT NULL,
+    `base_clock_speed` FLOAT(11) DEFAULT NULL,
+    `boost_clock_speed` FLOAT(11) DEFAULT NULL,
+    `retail_price` FLOAT(11) DEFAULT NULL,
     `additional_info` varchar(1000) DEFAULT NULL
 )ENGINE=InnoDB DEFAULT CHARSET=UTF8;
